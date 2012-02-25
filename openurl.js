1d2e34aeaa15f20983982658d424f67cef6a7f91
Sanat Gersappa <sanat.gersappa@gmail.com>
2012-02-25 17:31:01 +0530

    replaced exec with spawn; replaced start with explorer on win32 (tested with win7 32-bit) replaced linux2 with linux (tested on Linux Mint 32-bit)

--- openurl.js
+++ openurl.js
@@ -1,34 +1,35 @@
-var format = require("util").format;
-var exec = require('child_process').exec;
+var format = require('util').format,
+    spawn = require('child_process').spawn;
 
 var command;
 
-switch (process.platform) {
-    case "darwin":
-        command = "open '%s'";
-        break;
-    case "win32":
-        command = "start '%s'";
-        break;
-    case "linux2":
-        command = "xdg-open '%s'";
-        break;
-    default:
-        throw new Error("Unsupported platform: "+process.platform);
+switch(process.platform) {
+  case 'darwin':
+    command = 'open';
+    break;
+  case 'win32':
+    command = 'explorer.exe';
+    break;
+  case 'linux':
+    command = 'xdg-open';
+    break;
+  default:
+    throw new Error('Unsupported platform: ' + process.platform);
 }
-
 /**
  * Error handling is deliberately minimal, as this function is to be easy to use for shell scripting
  *
  * @param url The URL to open
  * @param callback A 0-argument function called after everything finishes successfully
  */
-exports.open = function (url, callback) {
-    exec(format(command, url), function (error, stdout, stderr) {
-        if (error) {
-            throw error;
-        } else {
-            callback();
-        }
-    });
+
+exports.open = function(url, callback) {
+  spawn(command, [url], function(error, stdout, stderr) {
+    if (error) {
+      throw error;
+    }
+    else {
+      callback();
+    }
+  });
 }
